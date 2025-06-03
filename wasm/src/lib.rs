91decd47a1b9a5523009fd3c4c4058f10dec0f1d
k22036 <k22036kk@aitech.ac.jp>
2025-06-03 15:49:49 +0900

    feat: add web-sys dependency and log greeting in greet function

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -2,9 +2,11 @@ mod types;
 
 use types::GreetResponse;
 use wasm_bindgen::prelude::*;
+use web_sys::console::log_1;
 
 #[wasm_bindgen]
 pub fn greet(name: Option<String>) -> GreetResponse {
+    log_1(&format!("greet called with name: {:?}", name).into());
     GreetResponse {
         value: "Hello, World!".to_string(),
         name,
